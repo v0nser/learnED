32adbc679c009d81058c4ac6b859cd5fd5138782
v0nser <raghuvanshivaibhav01@gmail.com>
2024-01-14 20:47:30 +0530

    improved ux

--- src/components/BlogComponents/Header/Header.jsx
+++ src/components/BlogComponents/Header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">LearnEd.</span>
-        <span className="headerTitleLg">Blog</span>
+        <span className="headerTitleLg">LearnEd.</span>
+        <span className="headerTitleSm">Pen Your Ideas!</span>
       </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt=""
       />
     </div>
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://learned.onrender.com'
+// export const BASE_URL = 'https://learned.onrender.com'
 // http://localhost:8000
+export const BASE_URL = 'http://localhost:8000'
